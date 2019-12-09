9a6120c7da48a77b17885b467fbd1be2739042c4
ElizabethTorre <etr604@gmail.com>
2019-12-09 18:52:58 -0500

    Updates

---
+++ precache-manifest.09197c9fbd5bbbe06b4a4ff3920f4baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aafee7e7b1b51ef32b6c893e752e42b",
+    "url": "/LIM010-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "a61a231ffa217f7a94aa",
+    "url": "/LIM010-fe-burger-queen/static/css/main.4f666bef.chunk.css"
+  },
+  {
+    "revision": "d4653c92d56242b72d84",
+    "url": "/LIM010-fe-burger-queen/static/js/2.87f34dfc.chunk.js"
+  },
+  {
+    "revision": "a61a231ffa217f7a94aa",
+    "url": "/LIM010-fe-burger-queen/static/js/main.7dac8e49.chunk.js"
+  },
+  {
+    "revision": "091cc3bacbdfcff80c68",
+    "url": "/LIM010-fe-burger-queen/static/js/runtime-main.7cddb1c7.js"
+  },
+  {
+    "revision": "eda230cd5774eb5998c2cada266429c8",
+    "url": "/LIM010-fe-burger-queen/static/media/logoBurger.eda230cd.png"
+  }
+]);
